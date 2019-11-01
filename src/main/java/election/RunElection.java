4ed0270f617fcc56602fc3d8ab3775fc5414d21f
Douglas Luman <dluman@douglasjluman.com>
2019-10-31 20:38:17 -0400

    Un-methodizing the Scanner.

--- src/main/java/election/RunElection.java
+++ src/main/java/election/RunElection.java
@@ -15,31 +15,24 @@ public class RunElection {
    * @param args The command line arguments.
    */
   public static void main(String[] args){
+    File file = null;
+    Scanner input = null;
+    try {
+      file = new File("inputs/candidates.list");
+      input = new Scanner(file);
+    } catch (FileNotFoundException noFile) {
+      System.exit(0);
+    }
+    return input;
+    }
     String name;
     Candidate candidate;
     Ballot ballot = new Ballot();
-    Scanner input = readFile("inputs/candidates.list");
     while (input.hasNext()) {
       name = input.nextLine();
       candidate = new Candidate(name);
       ballot.addCandidate(candidate);
     }
     System.out.print(ballot);
-  }
-  
-  /** Creates a blueprint for file Scanner objects.
-   *
-   * @param filename Relative file path.
-   */
-  public static Scanner readFile(String filepath) {
-    File file = null;
-    Scanner input = null;
-    try {
-      file = new File(filepath);
-      input = new Scanner(file);
-    } catch (FileNotFoundException noFile) {
-      System.exit(0);
-    }
-    return input;
   }
 }
